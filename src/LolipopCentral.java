81482e1f7985d1cc5796c545dfe28bca0e00a12a
edchau <edwarddchau@gmail.com>
2016-09-01 13:26:40 -0700

    Update LolipopCentral.java

--- src/LolipopCentral.java
+++ src/LolipopCentral.java
@@ -6,10 +6,37 @@ public class LolipopCentral {
 	
 	public static void main(String []args)
 	{
+		String mg;
 		Scanner scan = new Scanner (System.in);
 		LolipopGames lolipopGames = new LolipopGames();
 		lolipopGames.dice.rollDice();
-		
+		for(;;){
+			System.out.println("What mini-game would you like to play? Connect4, TicTacToe, or Dice Roll ");
+			mg=scan.nextString();
+			if(mg = "Connect4"){
+				cBoard();
+				printBoard();
+				System.out.println("Choose a column #1-7 to place your piece");
+
+				while (true) {
+					drop('X');
+					printBoard();
+					if (checkWin('X')) {
+						break;
+					}
+					if (isTied()) {
+						System.out.println("Game is tied");
+						break;
+					}
+					drop('0');
+					printBoard();
+					if (checkWin('0')) {
+						break;
+					}
+				}
+			}
+			
+		}
 	}
 	
 }
