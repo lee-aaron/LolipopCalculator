3f19b4c4720af5085cca31fa7aa5d23130d1aa3c
edchau <edwarddchau@gmail.com>
2016-09-24 23:14:03 -0700

    Update LolipopGames.java

--- src/LolipopGames.java
+++ src/LolipopGames.java
@@ -1,12 +1,10 @@
-
-public class LolipopGames {
-	
-	
+public class LolipopGames { //Allows access to games
 	public Connect4 connect = null;
-	
+	public TicTacToe ttt = null;
 	public LolipopGames() {
 		
 		connect = new Connect4();
+		ttt = new TicTacToe();
 	}
 	
 }
