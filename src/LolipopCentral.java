588c9aa32e112b728771fa86857a6e38d1eb3eaa
edchau <edwarddchau@gmail.com>
2016-09-24 23:13:35 -0700

    Update LolipopCentral.java

--- src/LolipopCentral.java
+++ src/LolipopCentral.java
@@ -1,4 +1,3 @@
-//import java.lang.reflect.Method;
 import java.util.*;
 
 public class LolipopCentral {
@@ -6,59 +5,51 @@ public class LolipopCentral {
 	
 	
 	public static void main(String []args) {
-		System.out.println("Welcome to LolipopCentral!" + "\n" + "Here, you can play minigames and earn " + "\n" + "Lollipops to buy items in the shop" + "\n");
-		
+		System.out.println("Welcome to LolipopCentral!" + "\n" + "Here, you can play mini-games and earn points (a.k.a Lollipops)." + "\n" + "Have Fun! :)" +"\n" + "P.S. Everything here is Text-Based");
 		LolipopGames lolipopGames = new LolipopGames();
-		LolipopSave lolipopSave = new LolipopSave();
+		//LolipopSave lolipopSave = new LolipopSave();
 		//Method m = LolipopSave.class.getDeclaredMethod("LolipopSave");
 		//m.setAccessible(true);
 		//m.invoke(lolipopSave);
-		double money = 10; // starting currency
 		Boolean settings = true;
-		
+		double points = 0; //point counter
 		while(settings){
 			System.out.println("What would you like to do? ");
-			System.out.println("\n" + "Game    Shop" + "\n" + "\n" + "Save    Lollipops" + "\n" + "\n" + "Quit" + "\n");
+			System.out.println("\n" + "Game    Save" + "\n" + "\n" + "Lollipops    Quit" + "\n" + "\n");
 			String opt = scan.next(); //option
 			if(opt.equalsIgnoreCase("Game")){ //access mini games
 				for(;;){
-					System.out.println("What mini-game would you like to play? Connect4, TicTacToe, or DiceRoll " + "\n" + "Enter 'Quit' to exit MiniGames" + "\n");
+					System.out.println("What mini-game would you like to play? Connect4 or TicTacToe " + "\n" + "Enter 'Quit' to exit MiniGames" + "\n");
 					String mg=scan.next(); //answer for mini game
 					if(mg.equalsIgnoreCase("Connect4")){
-						System.out.println("Enter 10 Lollipops to play (Y/N): " + "\n");
-						String Loli = scan.next(); //Enter Lolipop currency
-						if(Loli.equalsIgnoreCase("Y")){
-							money =- 10;
-							lolipopGames.connect.Connect();
-							continue;
-						}
-						else{
-							continue;
-						}
+						lolipopGames.connect.Connect();
+						continue;
 					}
+					
 				
 					else if(mg.equalsIgnoreCase("TicTacToe")){
+						lolipopGames.ttt.startMatch();
 						continue;
 					}
-					else if(mg.equalsIgnoreCase("")){
-						
-						continue;
+
+					else if(mg.equalsIgnoreCase("Quit")){
+						break;
 					}
 					else{
-						break;
+						System.out.println("Please enter a valid option: ");
+						mg=scan.next();
+						continue;
 					}
 				
 				}
 			}
-			else if(opt.equalsIgnoreCase("Shop")){ //shop
-			
-			}
+
 			else if(opt.equalsIgnoreCase("Save")){ //save progress
-				lolipopSave.addData(money);
+				//lolipopSave.addData(money);
 				System.out.println("Your game was saved successfully" + "\n");
 			}
 			else if(opt.equalsIgnoreCase("Lollipops")){ //currency
-				System.out.println("You have " + money + " lollipops" + "\n");
+				System.out.println("You have " + points + " lollipops" + "\n");
 			}
 			else if(opt.equalsIgnoreCase("Quit")){
 				settings = false;
